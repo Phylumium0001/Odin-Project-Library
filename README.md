daf0182cdbcc92371b0622fa93268bd780fd5c88
Phylumium <phylumium3000@gmail.com>
2024-05-24 09:05:33 +0000

    Complete Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Library Project
+This is one of the multiple projects curated by the Odin Project to reinforce our understanding of concepts learnt and also help us improve our problem solving capabilities
--- index.js
+++ index.js
@@ -1,73 +1,180 @@
-const myLibrary = [];
-
-function Book(author, title, pages,haveRead) {
-  this.Author = author,
-  this.Title = title,
-  this.Pages = pages,
-  this.haveRead = haveRead
-}
-const book1= new Book("J.K Rowlings", "Song of Ice and Fire", 369,false)
-myLibrary.push(book1)
-
-function addBookToLibrary(book) {
-  myLibrary.push(book)
-  console.log(myLibrary)
+function Book(title,author,pages,completionStatus){
+    changeStatus= (status)=>{
+        status ? completionStatus = false : completionStatus = true
+    }
+    return {title,author,pages,completionStatus,changeStatus}
 }
 
+function Library(){
+    let shelves = []
+    addBook = (bookObj)=>{
+        shelves.push(bookObj)
+    }
+    removeBook = (bookObj)=>{
+        for (const obj of shelves) {
+            if (obj['title'] === bookObj['title']){
+                let index = shelves.indexOf(obj)
+                shelves.splice(index,1)
+            }
+        }
+    }
+    checkBookAvailability=(bookObj)=>{
+        for (const book of shelves) {
+            if (bookObj.title === book.title){
+                return true
+            }
+        }
+    }
+    return {shelves,addBook,removeBook,checkBookAvailability}
+}
 
-function displayBooks() {
-    const oldNode = document.querySelector(".bookstore")
-    const bookstore = document.createElement("div")
-    bookstore.setAttribute("class", "bookstore")
-    
+// let book1 = Book("Harry","New",232,false)
+// let book2 = Book("Barry","Old",422,true)
+// let book3 = Book("John","Bro",532,false)
 
-    // Loop through the library array
-    for (const books of myLibrary){
-      const book = document.createElement("div")
-      book.setAttribute("class","book")
+let lib = Library()
+// lib.addBook(book1)
+// lib.addBook(book2)
+// lib.addBook(book3)
 
-      console.log("Display function")
-      // console.log(myLibrary[book])
-      let bookObj = books
-      const profilePic = document.createElement("img")
-      profilePic.setAttribute("src","./image_1714556660513_ujkw1a.svg")
-      book.appendChild(profilePic)
-      // Loop through the book object properties
-      Object.keys(bookObj).forEach(key=>{
-          const p = document.createElement("p")
-          p.textContent = `${key}: ${bookObj[key]}`
-          book.appendChild(p)
+function sideBar(){
+    const addBookBar = document.querySelector(".add-book-bar")
+    const openBtn = document.querySelector('.activateSidePane')
+    const closeBtn = document.querySelector('.deactivateSidePane')
+    activateSideBar=()=>{
+        // Add activate class to side bar
+        addBookBar.classList.add("activate")
+    }
+    deactivateSideBar=()=>{
+        // Add activate class to side bar
+        addBookBar.classList.remove("activate")
+    }
+    return {activateSideBar,deactivateSideBar}
+}
+
+function domManupulator(){
+    // Handles the completion state button
+    // progressBtnFunc = (e)=>{
+    //     console.log(e)
+    // }
+
+    // To be used by the shelves render function
+    removeAllItems= ()=>{
+        container = document.querySelector(".library-container")
+        container.innerHTML = ""
+    }
+    addBook = (bookObj)=>{
+        console.log("In")
+        container = document.querySelector(".library-container")
+        let bookCont = document.createElement("div")
+        bookCont.classList.add("book")
+        bookCont.dataset.completed = bookObj.completionStatus ? true : false
+        bookCont.dataset.title = bookObj.title
+
+        let imageCont = document.createElement("div")
+        imageCont.classList.add("image")
+        imageCont.textContent = "Image"
+        bookCont.appendChild(imageCont)
+
+        let bookTitle = document.createElement("div")
+        bookTitle.classList.add("bookTitle")
+        bookTitle.textContent = bookObj.title
+        bookCont.appendChild(bookTitle)
+
+        let bookAuthorCont = document.createElement("div")
+        bookAuthorCont.classList.add("bookAuthor")
+        bookAuthorCont.textContent = `Author:${bookObj.author}`
+        bookCont.appendChild(bookAuthorCont)
+
+        let bookPagesCont = document.createElement("div")
+        bookPagesCont.classList.add("bookPages")
+        bookPagesCont.textContent = `Pages:${bookObj.pages}`
+        bookCont.appendChild(bookPagesCont)
+
+        let bookBtnsCont = document.createElement("div")
+        bookBtnsCont.classList.add("bookBtns")
+
+        let completedBtnCont = document.createElement("button")
+        completedBtnCont.classList.add("completedBtn")
+        completedBtnCont.textContent = bookObj.completionStatus ? 'Completed' : "In Progess"
+        completedBtnCont.addEventListener('click',(e)=>{
+            // Reset the book object val
+            bookObj.completionStatus = bookObj.completionStatus ? false: true
+            // Reset the dataset val
+            e.target.parentNode.parentElement.dataset.completed = bookObj.completionStatus
+
+            completionBtn = e.originalTarget
+            completionBtn.textContent = bookObj.completionStatus ? "Completed": "In Progress"
+            console.log(bookObj.completionStatus)
         })
-        bookstore.appendChild(book)
-      }
+        
+        bookBtnsCont.appendChild(completedBtnCont)
 
+        let removeBtnCont = document.createElement("button")
+        removeBtnCont.classList.add("removeBtn")
+        removeBtnCont.textContent = "Remove"
+        removeBtnCont.addEventListener("click",(e)=>{
+            let domBookTitle = e.target.parentNode.parentElement.dataset.title
+            for (let index = 0; index < lib.shelves.length; index++) {
+                const element = lib.shelves[index];
+                if (element.title === domBookTitle){
+                    lib.shelves.splice(index,1)
+                }
+            }
+            removeAllItems()
+            renderShelve()
+        })
 
-    oldNode.parentNode.replaceChild(bookstore,oldNode)
-    // book.textContent = `${book1.title} by ${book1.author},${book1.pages} pages.`
-    
-    
+        bookBtnsCont.appendChild(removeBtnCont)
+
+        bookCont.append(bookBtnsCont)
+        
+        container.appendChild(bookCont)
+
+    }
+    renderShelve = ()=>{
+        removeAllItems()
+        for (const book of lib.shelves) {
+            addBook(book)
+        }
+    }
+    return {addBook,renderShelve}
 }
-document.addEventListener("DOMContentLoaded",displayBooks())
+man = domManupulator()
 
-const form = document.querySelector(".book-form")
-const submitBtn = document.querySelector(".submit")
-form.addEventListener("submit",(e)=>{
-  e.preventDefault()
+function handleForms() {
+    const title = document.getElementById("title").value
+    const author = document.getElementById("author").value
+    const pages = document.getElementById("pages").value
+    const genre = document.getElementById("genre").value
+    const completionStatus = document.querySelector("input[name='readStatus']:checked").value
+    // Check if Book already exists - Titles are unique
+    const newBook = Book(title,author,pages,completionStatus)
+    if (lib.checkBookAvailability(newBook)){
+        // If book does exist 
+        alert(`${title} already exists.`)
+    }else{
+        lib.addBook(newBook)
+        console.log(lib.shelves)
+        man.renderShelve()
+    }
+}
 
-  // Data from radio options
-  const data = new FormData(form)
-  console.log(data)
-  let title = data.get("book-title")
-  let author = data.get("book-author")
-  let pages = data.get("book-pages")
-  let haveRead = data.get("read-status")
+function addListerners(){
+    let sideBarObj = sideBar()
+    let openBtn = document.querySelector('.activateSidePane')
+    let closeBtn = document.querySelector('.deactivateSidePane')
+    let form = document.querySelector(".form-container")
+    openBtn.addEventListener("click",sideBarObj.activateSideBar)
+    closeBtn.addEventListener("click",sideBarObj.deactivateSideBar)
+    form.addEventListener("submit",handleForms)
+}
 
-  // Add Book to library array
-  addBookToLibrary(new Book(author,title,pages,haveRead))
-    
 
-submitBtn.addEventListener("clicked",(e)=>{form.submit()})
-})
-// Refreshing the library
-// const refresh = document.querySelector(".refresh")
-// refresh.addEventListener("clicked",displayBooks())
+// document.querySelector().addEventListener
+addListerners()
+
+// man.addBook(book1)
+// man.addBook(book2)
+// man.addBook(book3)
+// man.renderShelve(lib)
